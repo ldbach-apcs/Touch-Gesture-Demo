1e20f0ffe4b73990e1d20af321951005c01fe032
Le Duy Bach <ldbach@apcs.vn>
2018-04-26 10:04:43 +0700

    Improve scrolling experience

--- app/src/main/java/com/example/cpu02351_local/touchgesturedemo/CustomFrameLayout.kt
+++ app/src/main/java/com/example/cpu02351_local/touchgesturedemo/CustomFrameLayout.kt
@@ -2,12 +2,13 @@ package com.example.cpu02351_local.touchgesturedemo
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Color
+import android.os.Build
+import android.support.v4.math.MathUtils
 import android.util.AttributeSet
 import android.util.DisplayMetrics
-import android.util.Log
 import android.view.MotionEvent
 import android.view.VelocityTracker
+import android.view.View
 import android.view.ViewConfiguration
 import android.widget.FrameLayout
 
@@ -16,46 +17,71 @@ class CustomFrameLayout @JvmOverloads constructor(
 ) : FrameLayout(context, attrs, defStyleAttr) {
     private var vc = ViewConfiguration.get(context)!!
     private var mTouchSlop = 0
-    var isScrollingY = false
-    var isScrollingLeft = false
-    var firstX = -1
-    var firstY = -1
+    var downX = -1
+    var downY = -1
     var thresholdY = -1
     var thresholdX = -1
-    var halfScreen = -1
-    var mDis = 0
+    private var halfScreenWidth = -1
+    var halfScreenHeight= -1
+    var mDisX = 0
+    var mDisY = 0
+    private var mDirection: Int = DIRECTION_UNSPECIFIED
     private var mVelocityTracker = VelocityTracker.obtain()!!
     private var flingThreshold = -1
+    private lateinit var bg: View
+    private lateinit var mChildView: View
+    private lateinit var mScrollContainer: View
 
+    private var mMaxX = 0f
+    private var mMinX = 0f
+    private var mMaxY = 0f
+    private var mMinY = 0f
+
+    companion object {
+        const val DIRECTION_UP = 0
+        const val DIRECTION_DOWN = 1
+        const val DIRECTION_LEFT = 2
+        const val DIRECTION_RIGHT = 3
+        const val DIRECTION_UNSPECIFIED = 4
+    }
 
     init {
         this.mTouchSlop = vc.scaledTouchSlop
         flingThreshold = (vc.scaledMinimumFlingVelocity + vc.scaledMaximumFlingVelocity) / 10
         thresholdY = mTouchSlop
-        thresholdX = (mTouchSlop * 1.2).toInt() // Prioritize scrolling the list
+        thresholdX = mTouchSlop
         val displayMetrics = DisplayMetrics()
         (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
-        halfScreen = displayMetrics.widthPixels / 2
+        halfScreenWidth = displayMetrics.widthPixels / 2
+        halfScreenHeight = displayMetrics.heightPixels / 2
+    }
 
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        bg = getChildAt(0)
+        mChildView = getChildAt(1)
+        mScrollContainer = findViewById(R.id.scrollContainer)
+    }
+
+    override fun dispatchNestedScroll(dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, offsetInWindow: IntArray?): Boolean {
+        return super.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow)
     }
 
     override fun onInterceptTouchEvent(event: MotionEvent?): Boolean {
-        val movement = event!!.action
+        val movement = event!!.actionMasked
         mVelocityTracker.addMovement(event)
         when (movement) {
             MotionEvent.ACTION_DOWN -> {
                 resetFlags()
-                firstX = event.x.toInt()
-                firstY = event.y.toInt()
+                downX = event.rawX.toInt()
+                downY = event.rawY.toInt()
             }
-            MotionEvent.ACTION_MOVE -> when {
-                isScrollingVertical(event.y) -> {
-                    isScrollingY = true
-                }
-                isMovingLeft(event.x) -> {
-                    isScrollingLeft = true
-                    firstX = event.x.toInt()
-                    firstY = event.y.toInt()
+            MotionEvent.ACTION_MOVE -> {
+                mDirection = getMovingDirection(event)
+                if (mDirection != DIRECTION_UNSPECIFIED) {
+                    mScrollContainer.parent.requestDisallowInterceptTouchEvent(false)
+                    downX = event.rawX.toInt()
+                    downY = event.rawY.toInt()
                     return true
                 }
             }
@@ -63,78 +89,167 @@ class CustomFrameLayout @JvmOverloads constructor(
         return super.onInterceptTouchEvent(event)
     }
 
-    private fun isScrollingVertical(y: Float): Boolean =
-            !isScrollingLeft && Math.abs(y - firstY).toInt() >= thresholdY
-
-    private fun isMovingLeft(x: Float): Boolean = !isScrollingY && (x - firstX).toInt() >= thresholdX
+    private fun getMovingDirection(event: MotionEvent): Int {
+        val difX = event.rawX - downX
+        val difY = event.rawY - downY
+        return when {
+            difX > thresholdX && !mScrollContainer.canScrollHorizontally(-1) -> DIRECTION_RIGHT
+            difX < -thresholdX && !mScrollContainer.canScrollHorizontally(1) -> DIRECTION_LEFT
+            difY > thresholdY && !mScrollContainer.canScrollVertically(-1) -> DIRECTION_DOWN
+            difY < -thresholdY && !mScrollContainer.canScrollVertically(1) -> DIRECTION_UP
+            else -> DIRECTION_UNSPECIFIED
+        }
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val movement = event!!.action
+        val movement = event!!.actionMasked
         mVelocityTracker.addMovement(event)
 
         if (movement == MotionEvent.ACTION_MOVE) {
-            if (Math.abs(event.x - firstX).toInt() > thresholdX) {
-                translateLeft(event)
-                fadeBackground(event)
-                firstX = event.x.toInt()
+            when (mDirection) {
+                DIRECTION_RIGHT -> {
+                    mMaxX = (halfScreenWidth * 2).toFloat()
+                    mMinX = 0f
+                    mMinY = 0f
+                    mMaxY = 0f
+                }
+                DIRECTION_LEFT -> {
+                    mMaxX = 0f
+                    mMinX = (-halfScreenWidth * 2).toFloat()
+                    mMinY = 0f
+                    mMaxY = 0f
+                }
+                DIRECTION_UP -> {
+                    mMaxX = 0f
+                    mMinX = 0f
+                    mMinY = (-halfScreenHeight * 2).toFloat()
+                    mMaxY = 0f
+                }
+                DIRECTION_DOWN -> {
+                    mMaxX = 0f
+                    mMinX = 0f
+                    mMinY = 0f
+                    mMaxY = (halfScreenHeight * 2).toFloat()
+                }
             }
-            return true
-        }
-        else if (movement == MotionEvent.ACTION_UP) {
-            if (needReturnToPrev()) {
-                returnToPrev()
+            /*
+            if (stopInnerScroll) {
+                downX = event.rawX.toInt()
+                downY = event.rawY.toInt()
+
             } else {
-                getChildAt(1).animate()
-                        .translationX(0f)
-                        .withEndAction {
-                            getChildAt(0).alpha = 0f
-                        }
-                        .start()
+                mChildView.scrollBy(event.rawX.toInt() - downX, event.rawY.toInt() - downY)
+            }
+            */
+
+            fadeBackground(event)
+            translateChild(event)
+        }
+
+
+        if (movement == MotionEvent.ACTION_UP) {
+            if (needReturnToPrev()) {
+                smoothEndActivity()
+            } else {
+                resetState()
             }
         }
         return super.onTouchEvent(event)
     }
 
-    private fun returnToPrev() {
-        getChildAt(0).animate()
+    private fun resetState() {
+        mChildView.animate()
+                .translationX(0f)
+                .translationY(0f)
+                .withEndAction {
+                    bg.alpha = 0f
+                }
+                .start()
+    }
+
+    private fun translateChild(event: MotionEvent) {
+        mDisX = (event.rawX - downX).toInt()
+        mDisY = (event.rawY - downY).toInt()
+
+        mChildView.apply {
+            translationX = MathUtils.clamp(mDisX.toFloat(), mMinX, mMaxX)
+            translationY = MathUtils.clamp(mDisY.toFloat(), mMinY, mMaxY)
+        }
+    }
+
+
+    private fun smoothEndActivity() {
+        bg.animate()
                 .alpha(0f)
                 .start()
 
-        getChildAt(1).animate()
-                .translationX((halfScreen * 2).toFloat())
-                .withEndAction({
-                    (context as Main2Activity).finishNoAnim()
-                })
-                .start()
+        when (mDirection) {
+            DIRECTION_LEFT -> mChildView.animate()
+                    .translationX(mMinX)
+                    .withEndAction {
+                        (context as Main2Activity).finishNoAnim()
+                    }
+                    .start()
+
+            DIRECTION_RIGHT -> mChildView.animate()
+                    .translationX(mMaxX)
+                    .withEndAction {
+                        (context as Main2Activity).finishNoAnim()
+                    }
+                    .start()
+
+            DIRECTION_DOWN -> mChildView.animate().
+                    translationY(mMaxY)
+                    .withEndAction {
+                        (context as Main2Activity).finishNoAnim()
+                    }
+                    .start()
+
+            DIRECTION_UP -> mChildView.animate()
+                    .translationY(mMinY)
+                    .withEndAction {
+                        (context as Main2Activity).finishNoAnim()
+                    }
+                    .start()
+        }
     }
 
+    private val base = 255 * .75f
+    private val interpolateRange = 50
     private fun fadeBackground(event: MotionEvent) {
         //  interpolate transparency from 65% - 5%
         //  base - 20 * ratio
-        val base = 255 * .75f
-        val interpolateRange = 50
-        val ratio = event.x / (halfScreen * 1.4f)
+        val ratio = event.x / (halfScreenWidth * 1.4f)
         val alpha = (base - interpolateRange * ratio) / 255f
-        getChildAt(0).alpha = alpha
+        bg.alpha = alpha
     }
 
-    private fun translateLeft(event: MotionEvent) {
-        mDis += (event.x - firstX).toInt()
-        this.getChildAt(1).translationX = Math.max(0f, mDis.toFloat())
-    }
 
     private fun needReturnToPrev(): Boolean = isFlingDetected() || isSlideAcrossMiddle()
 
-    private fun isSlideAcrossMiddle(): Boolean = mDis >= halfScreen
+    private fun isSlideAcrossMiddle(): Boolean = when (mDirection) {
+        DIRECTION_LEFT -> mDisX <= -halfScreenWidth
+        DIRECTION_RIGHT -> mDisX >= halfScreenWidth
+        DIRECTION_DOWN -> mDisY >= halfScreenHeight
+        DIRECTION_UP -> mDisY <= -halfScreenHeight
+        else -> false
+    }
 
     private fun isFlingDetected(): Boolean {
         mVelocityTracker.computeCurrentVelocity(1000)
-        return mVelocityTracker.xVelocity >= flingThreshold
+        val velocX = mVelocityTracker.xVelocity
+        val velocY = mVelocityTracker.yVelocity
+        return when (mDirection) {
+            DIRECTION_LEFT -> velocX <= -flingThreshold
+            DIRECTION_RIGHT -> velocX >= +flingThreshold
+            DIRECTION_DOWN -> velocY >= flingThreshold
+            DIRECTION_UP -> velocY <= -flingThreshold
+            else -> false
+        }
     }
 
     private fun resetFlags() {
-        isScrollingY = false
-        isScrollingLeft = false
+        mDirection = DIRECTION_UNSPECIFIED
         mVelocityTracker.clear()
     }
 }
